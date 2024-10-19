683e4fbd90203e1ab9a9fcc38bbf703612f31fd7
E.K. <fake.hack376@gmail.com>
2024-10-19 14:40:31 -0700

    Create WordPage.tsx

---
+++ vision/src/pages/WordPage.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import Card from '../components/Card';
+
+const DictPage: React.FC = () => {
+    return (
+      <div className="card-container">
+        <Card letter="same" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlDuNdz3Bi3Bs9issz4gpFsJa089KYY5YUww&s" />
+        {/* Add more FlipCards as needed */}
+      </div>
+    );
+  };
+  
+  export default DictPage;
