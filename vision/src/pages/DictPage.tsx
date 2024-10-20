d4eb0ef191f6516010897754f2d6d91c6c7dd612
aghosh376 <arayboghosh@gmail.com>
2024-10-19 17:30:54 -0700

    added paths to images

--- vision/src/pages/DictPage.tsx
+++ vision/src/pages/DictPage.tsx
@@ -5,11 +5,11 @@ import './Dict.css';
 
 
 const cardData = [
-    { letter: 'A', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlDuNdz3Bi3Bs9issz4gpFsJa089KYY5YUww&s' },
-    { letter: 'B', imageUrl: 'https://plus.unsplash.com/premium_photo-1666284030814-9ed7d565ccf5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGFuZCUyMGdlc3R1cmVzfGVufDB8fDB8fHww' },
-    { letter: 'C', imageUrl: '../picture/imageC.jpg' },
-    { letter: 'D', imageUrl: '../picture/imageD.jpg' },
-    { letter: 'C', imageUrl: '../picture/imageC.jpg' },
+    { letter: 'A', imageUrl: '/assets/pictures/A.png' },
+    { letter: 'B', imageUrl: '/assets/pictures/B.png' },
+    { letter: 'C', imageUrl: '/assets/pictures/C.png' },
+    { letter: 'D', imageUrl: '/assets/pictures/D.png' },
+    { letter: 'E', imageUrl: '/assets/pictures/E.png' },
     { letter: 'D', imageUrl: '../picture/imageD.jpg' },
     { letter: 'C', imageUrl: '../picture/imageC.jpg' },
     { letter: 'D', imageUrl: '../picture/imageD.jpg' },
